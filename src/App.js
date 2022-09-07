79a00879ca093ade867b37a5ed7c1de001195e15
3oysen <103428192+3oysen@users.noreply.github.com>
2022-09-07 18:31:02 +0200

    functionality added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("hello wordl");
+		let amount = parseInt(count);
+		if (count <= 0) {
+			amount = 1;
+		}
+		if (count > 8) {
+			amount = 8;
+		}
+
+		setText(data.slice(0, amount));
 	};
 
 	return (
@@ -27,14 +35,9 @@ function App() {
 				</button>
 			</form>
 			<article className="lorem-text">
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo,
-					dolore?
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo,
-					dolore?
-				</p>
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
 			</article>
 		</section>
 	);
